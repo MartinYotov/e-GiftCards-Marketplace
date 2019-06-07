4d73d81d1574b7623896701c3a4901c0aca51a38
MartinYotov <m.yotov96@gmail.com>
2019-06-07 08:29:58 +0300

    added express

---
+++ server/app.js
@@ -0,0 +1,22 @@
+'use strict'
+const express = require('express');
+const port = require('./config').port;
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    
+    console.log(`Server is running on port: ${port}`);
+});
